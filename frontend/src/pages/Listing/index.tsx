6a3cf7e1129dbd1e20760a7c1239e223f1fe5ca5
matheusrodrigues18 <matheusfrank172@gmail.com>
2022-05-14 12:38:00 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,9 +28,11 @@ function Listing() {
         )
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) => { setPageNumber(newPageNumber); };
+
     return (
         <>
-            <Pagination />
+            <Pagination page = {page} onChange = {handlePageChange}/>
 
             <div className="container">
 
